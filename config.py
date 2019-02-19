506e59d973e2550531f5bf9cb4cbcef398465a3e
Shadowedvaca <shadowedvaca@gmail.com>
2019-02-18 19:09:36 -0700

    Merge all work to date

---
+++ config.py
@@ -0,0 +1,6 @@
+bot_api_token = "741904212:AAEHYZxX4i-TSek6KXPc70h6AIrYmvQe8c8"
+bot_name = "Complexify Bot"
+bot_username = "complexify_bot"
+bot_link = "tg://resolve?domain=complexify_bot"
+
+words_api_token = "d99e00040emsh5ab67fdb3e63bd7p17885ajsn11b8bbba988e"
